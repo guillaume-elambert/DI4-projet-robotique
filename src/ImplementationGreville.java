57f5b33ec8feaca41ded4941a205493e166fd59b
Guillaume ELAMBERT <guillaume.elambert@yahoo.fr>
2021-12-21 16:02:37 +0100

    Greville fonctionnel

--- src/ImplementationGreville.java
+++ src/ImplementationGreville.java
@@ -3,7 +3,7 @@ import Jama.Matrix;
 
 public class ImplementationGreville {
 	
-	static Matrix matA;
+static Matrix matA, resultat;
 	
 	public static void main(String[] args) {
 		
@@ -13,8 +13,9 @@ public class ImplementationGreville {
 			{0, 0, 4}
 		});
 		
-		
-		greville(matA);
+		resultat = greville(matA);
+		System.out.println("Résultat final :");
+		resultat.print(5,5);
 		
 		
 		matA = new Matrix(new double[][] {
@@ -22,78 +23,100 @@ public class ImplementationGreville {
 			{3, 1, 7}
 		});
 		
-		greville(matA);
+		resultat = greville(matA);
+		System.out.println("Résultat final :");
+		resultat.print(5,5);
 	}
 	
 	
 	static Matrix greville(Matrix matA) {
-		Matrix matAPlus, matAk, matAPlusk, matNulle, colNulle, ligneNulle, transpose, a, b, c, d, tmp;
-		int n, m;
+		
+
+		Matrix matAPlus, matAk, matAPlusk, transpose, a, b, c, d;
+		int n, m, rowMatAPlusk;
 		
 
 		n = matA.getRowDimension();
 		m = matA.getColumnDimension();
 		
-		matAPlus = new Matrix(n, m);
-		matNulle = new Matrix(n, m);
-		colNulle = new Matrix(n, 1);
-		ligneNulle = new Matrix(1, m);
+		matAPlus = new Matrix(1, n);
 		
 		
 		/*======== 1ere iteration ========*/
+
+		//On récupère la 1ère colonne
 		a = matA.getMatrix(0, n-1, 0, 0);
-		if(!a.equals(new Matrix(a.getRowDimension(), 1))) {
+
+		if(!a.equals(new Matrix(n, 1))) {
 			transpose = a.transpose();
 			matAPlusk = transpose.times(a).inverse().times(transpose);
-			matAPlus.setMatrix(0, 0, 0, m-1, matAPlusk);
+			
+			rowMatAPlusk = matAPlusk.getColumnDimension();
+			matAPlus.setMatrix(0, 0, 0, rowMatAPlusk-1, matAPlusk);
+
 		}
 		
 		double dDouble;
-		int tailleMat;
-		
+
 		for(int k = 1; k < m; ++k) {
-			
 			matAk = matA.getMatrix(0, n-1, 0, k-1);
-			matAPlusk = matAPlus.getMatrix(0, k-1, 0, m-1);
+			matAPlusk = matAPlus.getMatrix(0, k-1, 0, n-1);
 			
 			
 			a = matA.getMatrix(0, n-1, k, k);
+			
 			d = matAPlusk.times(a);
-			
-			
+
 			//Entrée : d est un chiffre et non une matrice
-			if(d.getRowDimension() == 1 && d.getColumnDimension() == 1) {
+			if (d.getRowDimension() == 1 && d.getColumnDimension() == 1) {
 				dDouble = d.get(0, 0);
 				c = a.minus(matAk.times(dDouble));
 			} else {
 				c = a.minus(matAk.times(d));
 			}
 
-			
+			//On vérifie si la matrice est nulle
+			int notNullCount = 0, cRow = c.getRowDimension(), cCol = c.getColumnDimension();
+			double laValeur;
+			for(int i = 0; i < cRow && notNullCount == 0; ++i){
+				for(int j = 0; j < cCol && notNullCount == 0; ++j){
+					laValeur=c.get(i,j);
+					if (laValeur != 0.0) {
+						//On fait une approxiamtion à 10^-10
+						if ( ((long)laValeur * 1e10) / 1e10 == 0.0) {
+							c.set(i, j, 0.0);
+							break;
+						}
+						++notNullCount;
+					}
+				}
+			}
+
+
+
 			//Entrée : c'est une matrice nulle
-			if(c.equals(new Matrix(c.getRowDimension(), 1))) {
+			if(notNullCount == 0) {
 				transpose = d.transpose();
-				b = Matrix.identity(n, m).plus(transpose.times(d));
+				b = transpose.times(d);
+				b = Matrix.identity(b.getRowDimension(), b.getColumnDimension()).plus(b);
 				b = b.inverse().times(transpose).times(matAPlusk); 
 			} else {
 				transpose = c.transpose();
 				b = transpose.times(c).inverse().times(transpose);
 			}
-			
+
 			matAPlusk = matAPlusk.minus(d.times(b));
-			tailleMat = matAPlusk.getRowDimension();
+			rowMatAPlusk = matAPlusk.getRowDimension();
+
+
 			
+			Matrix tmp = new Matrix(b.getRowDimension()+matAPlus.getRowDimension(), matAPlus.getColumnDimension());
+			tmp.setMatrix(0, matAPlus.getRowDimension()-1, 0, matAPlus.getColumnDimension()-1, matAPlus);
+			matAPlus = tmp;
 			
-			matAPlus.setMatrix(0, tailleMat-1, 0, m-1, matAPlusk);
-			matAPlus.setMatrix(tailleMat, tailleMat, 0, m-1, b);
-			
-			System.out.println("\n\nIt�ration "+(k+1));
-			System.out.print("\nA+k:");
-			matAPlusk.print(0, 5);
-			System.out.print("b:");
-			b.print(0, 5);
-			System.out.print("A+:");
-			matAPlus.print(0, 5);
+			matAPlus.setMatrix(0, rowMatAPlusk-1, 0, n-1, matAPlusk);
+			matAPlus.setMatrix(rowMatAPlusk, rowMatAPlusk, 0, b.getColumnDimension()-1, b);
+
 		}
 		
 		return matAPlus;
